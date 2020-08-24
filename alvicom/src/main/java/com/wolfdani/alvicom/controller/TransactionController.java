e550bce8c834f42733aa6f71b9a38415b90b8806
Wolf Daniel <wolfdani@freemail.hu>
2020-08-24 16:01:10 +0200

    dependency

--- alvicom/src/main/java/com/wolfdani/alvicom/controller/TransactionController.java
+++ alvicom/src/main/java/com/wolfdani/alvicom/controller/TransactionController.java
@@ -3,10 +3,7 @@ package com.wolfdani.alvicom.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
